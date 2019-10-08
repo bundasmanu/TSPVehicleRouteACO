aba84b2f7925406bb2e9ce5594b2100da3b31894
bundamanu <gustavo.abcdef@hotmail.com>
2019-10-08 22:51:44 +0100

    Alteracoes

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 import acopy
 import Utils
 
+def main():
+
+    print(Utils.Utils.dictOfNodes)
+
+if __name__ == "__main__":
+    main()
--- Utils.py
+++ Utils.py
@@ -1,3 +1,4 @@
+from typing import List
 
 class Utils:
 
@@ -6,35 +7,50 @@ class Utils:
     FEROMONERATE = "taxaFeromona"
     DISTANCE = "distancia"
     EDGES = "edges"
+    EDGE1 = "edge1"
+    EDGE2 = "edge2"
+    EDGE3 = "edge3"
+    EDGE4 = "edge4"
+    NODEA = "nodeA"
+    NODEB = "nodeB"
+    NODEC = "nodeC"
+    NODED = "nodeD"
+    NODEE = "nodeE"
 
-    dict={INITIALPOINT : 'A', EDGES : {
-                                    {INITIALPOINT: 'A', FINALPOINT: 'B', DISTANCE: 2, FEROMONERATE : 0.1},
-                                    {INITIALPOINT: 'A', FINALPOINT: 'C', DISTANCE: 5, FEROMONERATE: 0.1},
-                                    {INITIALPOINT: 'A', FINALPOINT: 'D', DISTANCE: 5, FEROMONERATE: 0.2},
-                                    {INITIALPOINT: 'A', FINALPOINT: 'E', DISTANCE: 5, FEROMONERATE: 0.2}
-        },
-        INITIALPOINT: 'B', EDGES: {
-                                    {INITIALPOINT: 'B', FINALPOINT: 'A', DISTANCE: 2, FEROMONERATE: 0.1},
-                                    {INITIALPOINT: 'B', FINALPOINT: 'C', DISTANCE: 2, FEROMONERATE: 0.1},
-                                    {INITIALPOINT: 'B', FINALPOINT: 'D', DISTANCE: 4, FEROMONERATE: 0.2},
-                                    {INITIALPOINT: 'B', FINALPOINT: 'E', DISTANCE: 6, FEROMONERATE: 0.3}
-        },
-        INITIALPOINT: 'C', EDGES: {
-                                    {INITIALPOINT: 'C', FINALPOINT: 'A', DISTANCE: 5, FEROMONERATE: 0.1},
-                                    {INITIALPOINT: 'C', FINALPOINT: 'B', DISTANCE: 2, FEROMONERATE: 0.1},
-                                    {INITIALPOINT: 'C', FINALPOINT: 'D', DISTANCE: 5, FEROMONERATE: 0.5},
-                                    {INITIALPOINT: 'C', FINALPOINT: 'E', DISTANCE: 5, FEROMONERATE: 0.1}
-        },
-        INITIALPOINT: 'D', EDGES: {
-                                    {INITIALPOINT: 'D', FINALPOINT: 'A', DISTANCE: 5, FEROMONERATE: 0.2},
-                                    {INITIALPOINT: 'D', FINALPOINT: 'B', DISTANCE: 4, FEROMONERATE: 0.2},
-                                    {INITIALPOINT: 'D', FINALPOINT: 'C', DISTANCE: 5, FEROMONERATE: 0.5},
-                                    {INITIALPOINT: 'D', FINALPOINT: 'E', DISTANCE: 6, FEROMONERATE: 0.6}
-        },
-        INITIALPOINT: 'E', EDGES: {
-                                    {INITIALPOINT: 'E', FINALPOINT: 'A', DISTANCE: 3, FEROMONERATE: 0.2},
-                                    {INITIALPOINT: 'E', FINALPOINT: 'B', DISTANCE: 6, FEROMONERATE: 0.3},
-                                    {INITIALPOINT: 'E', FINALPOINT: 'C', DISTANCE: 5, FEROMONERATE: 0.1},
-                                    {INITIALPOINT: 'E', FINALPOINT: 'D', DISTANCE: 6, FEROMONERATE: 0.6}
-        },
+    dictA = {INITIALPOINT : 'A', EDGE1 : {INITIALPOINT: 'A', FINALPOINT: 'B', DISTANCE: 2, FEROMONERATE : 0.1},
+                               EDGE2 : {INITIALPOINT: 'A', FINALPOINT: 'C', DISTANCE: 5, FEROMONERATE: 0.1},
+                               EDGE3 : {INITIALPOINT: 'A', FINALPOINT: 'D', DISTANCE: 5, FEROMONERATE: 0.2},
+                               EDGE4 : {INITIALPOINT: 'A', FINALPOINT: 'E', DISTANCE: 5, FEROMONERATE: 0.2}
+    }
+
+    dictB = {INITIALPOINT: 'B', EDGE1: {INITIALPOINT: 'B', FINALPOINT: 'A', DISTANCE: 2, FEROMONERATE: 0.1},
+                                EDGE2 : {INITIALPOINT: 'B', FINALPOINT: 'C', DISTANCE: 2, FEROMONERATE: 0.1},
+                                EDGE3 : {INITIALPOINT: 'B', FINALPOINT: 'D', DISTANCE: 4, FEROMONERATE: 0.2},
+                                EDGE4 : {INITIALPOINT: 'B', FINALPOINT: 'E', DISTANCE: 6, FEROMONERATE: 0.3}
+    }
+
+    dictC = {INITIALPOINT: 'C', EDGE1: {INITIALPOINT: 'C', FINALPOINT: 'A', DISTANCE: 5, FEROMONERATE: 0.1},
+                                EDGE2 :  {INITIALPOINT: 'C', FINALPOINT: 'B', DISTANCE: 2, FEROMONERATE: 0.1},
+                                EDGE3 : {INITIALPOINT: 'C', FINALPOINT: 'D', DISTANCE: 5, FEROMONERATE: 0.5},
+                                EDGE4 :  {INITIALPOINT: 'C', FINALPOINT: 'E', DISTANCE: 5, FEROMONERATE: 0.1}
+    }
+
+    dictD = {INITIALPOINT: 'D', EDGE1: {INITIALPOINT: 'D', FINALPOINT: 'A', DISTANCE: 5, FEROMONERATE: 0.2},
+                                EDGE2 : {INITIALPOINT: 'D', FINALPOINT: 'B', DISTANCE: 4, FEROMONERATE: 0.2},
+                                EDGE3 : {INITIALPOINT: 'D', FINALPOINT: 'C', DISTANCE: 5, FEROMONERATE: 0.5},
+                                EDGE4 : {INITIALPOINT: 'D', FINALPOINT: 'E', DISTANCE: 6, FEROMONERATE: 0.6}
+    }
+
+    dictE = {INITIALPOINT: 'E', EDGE1: {INITIALPOINT: 'E', FINALPOINT: 'A', DISTANCE: 3, FEROMONERATE: 0.2},
+                                EDGE2 : {INITIALPOINT: 'E', FINALPOINT: 'B', DISTANCE: 6, FEROMONERATE: 0.3},
+                                EDGE3 : {INITIALPOINT: 'E', FINALPOINT: 'C', DISTANCE: 5, FEROMONERATE: 0.1},
+                                EDGE4 : {INITIALPOINT: 'E', FINALPOINT: 'D', DISTANCE: 6, FEROMONERATE: 0.6}
+    }
+
+    dictOfNodes = {
+        NODEA : dictA,
+        NODEB: dictB,
+        NODEC: dictC,
+        NODED: dictD,
+        NODEE: dictE,
     }
